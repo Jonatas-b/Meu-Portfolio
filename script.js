6bafbc1b46e413d11728512e43145101356d0481
Jônatas Emanuel Batista <jonatasemanuelbatista12@gmail.com>
2025-10-05 10:35:53 -0300

    Atualização nos 3 arquivos base
    Escureci o verde da página e mexi na responsividade dos links de contato e dos cards dos projetos

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ document.querySelectorAll("section").forEach((section) => {
   observer.observe(section);
 });
 
+// Função para animar o logo em loop
 function loopLogoAnimation() {
   const logo = document.querySelector('.logo');
   logo.classList.add('animating');
@@ -25,6 +26,7 @@ function loopLogoAnimation() {
 }
 loopLogoAnimation();
 
+// Função para efeito de máquina de escrever no texto
 function typeWriter(element, text, speed = 30) {
   element.innerHTML = ""; // Limpa o conteúdo antes de digitar
   let i = 0;
@@ -38,14 +40,16 @@ function typeWriter(element, text, speed = 30) {
   typing();
 }
 
+// Executa quando o DOM estiver carregado
 document.addEventListener("DOMContentLoaded", function() {
   const sobreTexto = document.getElementById("sobre-texto");
   if (sobreTexto) {
-    const texto = `Tenho 17 anos e atualmente estou no terceiro ano do ensino médio integrado ao curso técnico em Desenvolvimento de Sistemas, onde venho desenvolvendo minha base na área da programação e na construção de soluções digitais.
+    // Texto a ser exibido com efeito de máquina de escrever
+    const texto = `Tenho 18 anos e atualmente estou no terceiro ano do ensino médio integrado ao curso técnico em Desenvolvimento de Sistemas, onde venho desenvolvendo minha base na área da programação e na construção de soluções digitais.
 Ao longo da minha formação, participei de projetos práticos. Essas experiências me proporcionaram contato direto com conceitos importantes de lógica, estruturação de código e interface com o usuário, despertando ainda mais meu interesse pelo desenvolvimento de software.
 Embora ainda esteja em processo de aprofundamento, busco constantemente aprender novas linguagens e tecnologias. Tenho interesse tanto no front-end quanto no back-end, e acredito na importância de desenvolver uma base versátil para aproveitar oportunidades em diversas áreas da programação.
 Me destaco por minha facilidade e disponibilidade para aprender, espírito de liderança, boa comunicação e resiliência frente aos desafios. Sou comprometido, levo a sério tudo aquilo que me proponho a fazer e gosto de atuar em equipe, colaborando para que ideias se transformem em resultados concretos.
 Meu objetivo é crescer como desenvolvedor, ampliando meus conhecimentos e competências para atuar em projetos reais, contribuindo para soluções inovadoras em um mercado em constante evolução.`;
-    typeWriter(sobreTexto, texto, 18); // ajuste a velocidade se quiser
+    typeWriter(sobreTexto, texto, 18);
   }
 });
